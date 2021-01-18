70988cc2ec2e5ebf24a883ec44d98bf5d1406045
Aarox3 <poiuytrewq1990@o2.pl>
2021-01-18 11:12:17 +0100

    Space between int userInt and while is deleted.

---
+++ Gierka/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gierka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+          
+            int randomInt = rand.Next(101);
+
+            int counter = 1;
+            while (true)
+            {
+                
+
+             try { 
+            Console.WriteLine("Komputer losuje liczbę od 0 do 100. Wpisz jaką Twoim zdaniem, CPU wybrał.");
+                    
+
+                    int userInt = int.Parse(Console.ReadLine());
+
+                  
+
+             while (randomInt != userInt)
+            {
+
+                if(randomInt > userInt)
+                {
+                    Console.WriteLine("Nie odgadnąłeś liczby. Jest ona większa, próbuj dalej.");
+                }
+                if(randomInt < userInt)
+                {
+                    Console.WriteLine("Nie odgadnąłeś liczby. Jest ona mniejsza, próbuj dalej.");
+                }
+
+
+                userInt = int.Parse(Console.ReadLine());
+                counter++;
+
+            }
+            Console.WriteLine("Odgadnąłeś liczbę w " + counter + " podejściach, brawo!");
+                    break;
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Podana wartość jest niewłaściwa, następnym razem wpisz liczbę!");
+                    counter++;
+            }
+            }
+
+        }
+        }
+    }
+
+   
+
